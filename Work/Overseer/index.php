8afdcf0f7e43f9e3f17392d61d392dccc0da3771
Isabella Dean <Bean6754@users.noreply.github.com>
2019-09-20 15:56:22 +0100

    Update index.php

--- Work/Overseer/index.php
+++ Work/Overseer/index.php
@@ -1,93 +1,28 @@
 <!DOCTYPE html>
-<html lang="en-GB">
+<html lang="he-GB">
 <head>
     <meta charset="UTF-8">
     <title>Overseer</title>
     <link rel="stylesheet" type="text/css" href="/css/overseer.css">
+    <script>
+        setTimeout(function(){
+           window.location.reload(1);
+        }, 2000);
+    </script>
+    <!-- <script src="/js/jquery-3.4.1.min.js"></script> -->
 </head>
 <body>
-<?php
+    <center><div id="dateDiv"></div></center>
+    <center><div id="timeDiv"></div></center>
+    <script>
+        var today = new Date();
+        var optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        var optionsTime = { timeZone: 'Europe/London', hour: 'numeric', minute: 'numeric', timeZoneName: 'long' };
 
-// Status messages.
-$statusOnlineMsg = '<p style="color: #080;">Online</p>';
-$statusOfflineMsg = '<p style="color: #800;">Offline</p>';
-
-// For 10.44.103.181.
-
-echo "<h2>10.44.103.181 - Cell 1B eMaint</h2>";
-
-$host = '10.44.103.181';
-$port = 2222;
-$waitTimeoutInSeconds = 1;
-if ($fp = fsockopen($host, $port, $errCode, $errStr, $waitTimeoutInSeconds))
-{
-    // Works.
-    echo $statusOnlineMsg;
-}
-else
-{
-    // It does not work.
-    echo $statusOfflineMsg;
-}
-fclose($fp);
-
-// For 10.44.103.183.
-
-echo "<h2>10.44.103.183 - Cell 1A CCTV</h2>";
-
-$host = '10.44.103.183';
-$port = 2222;
-$waitTimeoutInSeconds = 1;
-if ($fp = fsockopen($host, $port, $errCode, $errStr, $waitTimeoutInSeconds))
-{
-    // Works.
-    echo $statusOnlineMsg;
-}
-else
-{
-    // It does not work.
-    echo $statusOfflineMsg;
-}
-fclose($fp);
-
-// For 10.44.103.184.
-
-echo "<h2>10.44.103.184 - Training Room</h2>";
-
-$host = '10.44.103.184';
-$port = 2222;
-$waitTimeoutInSeconds = 1;
-if ($fp = fsockopen($host, $port, $errCode, $errStr, $waitTimeoutInSeconds))
-{
-    // Works.
-    echo $statusOnlineMsg;
-}
-else
-{
-    // It does not work.
-    echo $statusOfflineMsg;
-}
-fclose($fp);
-
-// For 10.44.103.185.
-
-echo "<h2>10.44.103.185 - IT Office Overseer</h2>";
-
-$host = '10.44.103.185';
-$port = 2222;
-$waitTimeoutInSeconds = 1;
-if ($fp = fsockopen($host, $port, $errCode, $errStr, $waitTimeoutInSeconds))
-{
-    // Works.
-    echo $statusOnlineMsg;
-}
-else
-{
-    // It does not work.
-    echo $statusOfflineMsg;
-}
-fclose($fp);
-
-?>
+        document.getElementById('dateDiv').innerHTML = "<p>" + today.toLocaleDateString("he-GB-u-ca-gregory", optionsDate) + "</p>";
+        document.getElementById('timeDiv').innerHTML = "<p>" + today.toLocaleTimeString("he-GB", optionsTime) + "</p>";
+    </script>
+    
+    <?php include('includes/status-he.php'); ?>
 </body>
 </html>
