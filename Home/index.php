871f6f1942143735a3530f2a693177c2c374d63d
Isabella Dean <Bean6754@users.noreply.github.com>
2019-10-22 16:14:17 +0100

    Create index.php

---
+++ Home/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/global.css">
+</head>
+<body>
+    <?php
+        include("inc/desktop.php");
+    ?>
+</body>
+</html>
