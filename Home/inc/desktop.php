c89f880fe401a2e48eb0db494582d289162bb999
Isabella Dean <Bean6754@users.noreply.github.com>
2019-10-22 16:15:05 +0100

    Create desktop.php

---
+++ Home/inc/desktop.php
@@ -0,0 +1,28 @@
+<style>
+    .button {
+        padding-right: 16px;
+    }
+    .button:hover {
+        opacity: 0.8;
+        filter: alpha(opacity=80); /* For IE8 and earlier */
+    }
+    .button_img {
+        border-radius: 25%;
+        border: 1px solid #222222;
+    }
+</style>
+
+<div id="desktop">
+    <a href="index.php" class="button">
+        <!--
+            Home Page
+        -->
+        <img class="button_img" id="img_desktop" src="img/icons/desktop.jpg" width="100" height="100">
+    </a>
+    <a href="https://duckduckgo.com" target="_blank" class="button">
+        <!--
+            DuckDuckGo
+        -->
+        <img class="button_img" id="img_duck" src="img/icons/duck.png" width="100" height="100">
+    </a>
+</div>
